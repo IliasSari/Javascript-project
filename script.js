0c4a6a108788c4c65ddff203c295b5061ff424ba
IliasSari <sarigilias@gmail.com>
2026-03-18 18:54:52 +0200

    Made changes to html and js

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
-const title = document.getElementById("title");
-const button = document.getElementById("myButton");
+const myNewButton = document.createElement("button");
 
-button.addEventListener("click", function() {
-    title.innerText = "Button pressed! 🎉";
-    title.style.color = "blue";
-});
+myNewButton.innerText = "Press(Made with JS!)";
+myNewButton.style.padding = "20px";
+myNewButton.style.fontSize = "20px";
+myNewButton.style.cursor = "pointer";
 
-title.addEventListener("mouseover", function() {
-    title.style.fontSize = "50px";
-    title.style.transition = "0.5s"; 
-});
 
-title.addEventListener("mouseout", function() {
-    title.style.fontSize = "32px";
-    title.style.color = "black";
+function randomColor(){
+    let r = Math.floor(Math.random()* 256);
+    let g = Math.floor(Math.random()* 256);
+    let b = Math.floor(Math.random()* 256);
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
+myNewButton.addEventListener("click", function() {
+    const newColor = randomColor();
+
+    document.body.style.backgroundColor = newColor;
+    console.log("New color is: "+ newColor);
 });
+document.body.appendChild(myNewButton);
