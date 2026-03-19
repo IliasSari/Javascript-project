92165af55f73726c6ecd02f15798d03b8be17239
IliasSari <sarigilias@gmail.com>
2026-03-19 11:47:20 +0200

    Added click counter

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
+let counter = 0;
 const myNewButton = document.createElement("button");
 
 myNewButton.innerText = "Press(Made with JS!)";
-myNewButton.style.padding = "20px";
-myNewButton.style.fontSize = "20px";
+myNewButton.style.padding = "13px";
+myNewButton.style.fontSize = "13px";
 myNewButton.style.cursor = "pointer";
-
+document.body.appendChild(myNewButton);
 
 function randomColor(){
     let r = Math.floor(Math.random()* 256);
@@ -15,8 +16,17 @@ function randomColor(){
 
 myNewButton.addEventListener("click", function() {
     const newColor = randomColor();
+    counter = counter + 1;
+    myNewButton.innerText = "Clicks: " + counter;
+
+    if (counter === 10){
+        myNewButton.style.backgroundColor = "red";
+        myNewButton.innerText = "10!!!";
+    }
 
     document.body.style.backgroundColor = newColor;
-    console.log("New color is: "+ newColor);
+    console.log("New color is: "+ newColor);    
 });
+
+
 document.body.appendChild(myNewButton);
